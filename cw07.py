1f954762887a850e9e045446acf8587e6ec7ae30
Amelia Roseto <roseto@chapman.edu>
2018-10-18 17:27:04 +0000

    ya baby

--- cw07.py
+++ cw07.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 ###
-# Name: YOUR_FULL_NAME_HERE
-# Student ID: ID_HERE
-# Email: CHAPMAN_EMAIL_HERE
+# Name: Amelia Roseto & Gwyneth Casey & Gabriella Nutt
+# Student ID: 2289652
+# Email: roseto@chapman.edu
 # Course: PHYS220/MATH220/CPSC220 Fall 2018
-# Assignment: HOMEWORK_OR_CLASSWORK_NUMBER
+# Assignment: CW07
 ###
 
 """Classwork 07
@@ -17,7 +17,7 @@ python lists.
 import math
 import numpy as np
 
-def gen_gaussian_list(a, b, n=1000):
+def sinc_list(a, b, n=1000):
     """gen_gaussian_list(a, b, n=1000)
     Generate a discrete approximation of a Gaussian function, including its
     domain and range, stored as a pair of vanilla python lists.
@@ -32,20 +32,22 @@ def gen_gaussian_list(a, b, n=1000):
             x  : [a, ..., b] List of n equally spaced floats between a and b
             g  : [g(a), ..., g(b)] List of Gaussian values matched to x
     """
-    dx = (b-a)/(n-1)                         # spacing between points
-    x = [a + k*dx for k in range(n)]         # domain list
-    
-    # Local implementation of a Gaussian function
-    def gauss(x):
-        return (1/math.sqrt(2*math.pi))*math.exp(-x**2/2)
-    
-    g = [gauss(xk) for xk in x]                  # range list
-    return (x, g)
+    dx = (b-a)/(n-1)
+    x = [a + k*dx for k in range(n)]
+
+    def sinc(x):
+        if x == 0:
+            return 1
+        else:
+            return (math.sin(x) / x)
+
+    sinc = [sinc(xk) for xk in x]
+    return (x, sinc)
 
 
-def gen_gaussian_array(a, b, n=1000):
-    """gen_gaussian_array(a, b, n=1000)
-    Generate a discrete approximation of a Gaussian function, including its
+def sinc_array(a, b, n=1000):
+    """sinc_array(a, b, n=1000)
+    Generate a discrete approximation of a sinc function, including its
     domain and range, stored as a pair of numpy arrays.
     
     Args:
@@ -54,43 +56,59 @@ def gen_gaussian_array(a, b, n=1000):
         n (int, optional) : Number of points in domain, defaults to 1000.
     
     Returns:
-        (x, g) : Pair of numpy arrays of float64
+        (x, s) : Pair of numpy arrays of float64
             x  : [a, ..., b] Array of n equally spaced float64 between a and b
-            g  : [g(a), ..., g(b)] Array of Gaussian values matched to x
+            s  : [s(a), ..., s(b)] Array of sinc values matched to x
     """
-    pass
 
 
-def main(a,b,n=1000):
-    """main(a, b, n=1000)
-    Main function for command line operation. Prints result of Gaussian to screen.
-    
+    x = np.linspace(a, b, endpoint=True, num=n)
+
+    def sinc(x):
+        ones = np.ones_like(x)
+        return(np.divide(np.sin(x), x, out = ones, where=x!=0))
+
+    s = sinc(x)
+    return (x, s)
+
+def gen_sinf_list(a, b, n=1000):
+    """
     Args:
         a (float) : Lower bound of domain
         b (float) : Upper bound of domain
         n (int, optional) : Number of points in domain, defaults to 1000.
-    
     Returns:
-        None
-    
-    Effects:
-        Prints Gaussian to screen.
+        (x, g) : Pair of lists of floats
+        x  : [a, ..., b] List of n equally spaced floats between a and b
+        g  : [g(a), ..., g(b)] List of sinf values matched to x
     """
-    # You can unpack tuples as return values easily
-    x, g = gen_gaussian_list(a,b,n)
-    # The zip function takes two lists and generates a list of matched pairs
-    for (xk, gk) in zip(x, g):
-        # The format command replaces each {} with the value of a variable
-        print("({}, {})".format(xk, gk))
+    dx = (b-a)/(n-1)                         # spacing between points
+    x = [a + k*dx for k in range(n)]         # domain list
+    x = [z for z in x if z != 0]
 
+    def sinf(x):
+        return (math.sin(1/x))
 
-# Protected main block for command line operation
+    g = [sinf(xk) for xk in x]
+    return (x, g)
+
+def sinf_array(a, b, n=1000):
+
+    x = np.linspace(a,b,endpoint=True,num=n)
+    g = np.ones_like(x)
+    g = np.sin(np.divide(1, x, where=x!=0, out=g))
+    return (x, g)
+
+def main(a,b,n=1000):
+
+    x1, g1 = sinc_list(a,b,n)
+
+    for (x1k, g1k) in zip(x1, g1):
+
+        print("({}, {})".format(x1k, g1k))
+        
 if __name__ == "__main__":
-    # The sys module contains features for running programs
     import sys
-    # The sys.argv list variable contains all command line arguments
-    #    sys.argv[0] is the program name always
-    #    sys.argv[1] is the first command line argument, etc
     if len(sys.argv) == 4:
         a = float(sys.argv[1])
         b = float(sys.argv[2])
